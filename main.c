712c547d473ffa89f7769503bae9b2bbebacc7b0
Jair Escamilla <hernandez_yo14@outlook.es>
2019-11-18 16:09:21 -0600

    Codificando morse

--- main.c
+++ main.c
@@ -20,14 +20,14 @@ int main(void){
             return 0;
         }else{
             // Manda Ejecutar la funcion machine.process() correspondiente a la etiqueta machine_state.
-                (*machine[machine_state].process)(&machine_state, Raiz);
+                (*machine[machine_state].process)(&machine_state, &Raiz);
         }
     }
 
     return 0;
 }
 
-void load_file(States* state, TipoNodo* Raiz){
+void load_file(States* state, TipoNodo** Raiz){
     printf("Cargando archivo...");
     FILE *Archivo;
     char Renglon[100], Letra;
@@ -41,14 +41,14 @@ void load_file(States* state, TipoNodo* Raiz){
         while (fgets(Renglon, 50, Archivo) != NULL){
             Renglon[strlen(Renglon)] = '\0';
             sscanf(Renglon, "%c, %lf", &Letra, &Probabilidad);
-            InsertarArbol(&Raiz, Probabilidad, Letra);
+            InsertarArbol(Raiz, Probabilidad, Letra);
         }
     }
     fclose(Archivo);
     *state = WAIT;
     printf("Archivo cargado!\n");
 }
-void wait(States* state, TipoNodo* Raiz){
+void wait(States* state, TipoNodo** Raiz){
     int opcion;
     printf("(1) Codificar mensaje.\n(2) Decodificar mensaje.\n(3) Salir.\nIngresar opcion-> ");
     scanf("%d", &opcion);
@@ -66,24 +66,64 @@ void wait(States* state, TipoNodo* Raiz){
             printf("Esta opcion no existe!");
     }
 }
-void encode(States* state, TipoNodo* Raiz){
+void encode(States* state, TipoNodo** Raiz){
+    char mensaje[150], codificado[150], mensajeCodificado[150];
+    int i = 0, flag = 0;
+    printf("Ingresar frase que desea codificar a morse-> ");
+    scanf(" %s", mensaje);
+    minusculas(mensaje);
     printf("Codificando mensaje...");
-    //print_process_bar(5);
+    puts("Mensaje codificado con exito!\n");
+    printf("\nEl mensaje condificado es: ");
+    while(mensaje[i] != '\0'){
+        codificar(mensaje[i], codificado, 0, *Raiz, &flag, mensajeCodificado);
+        flag = 0;
+        printf("%s ", mensajeCodificado);
+        codificado[0] = '\0';
+        i++;
+    }
+    printf("\n");
     *state = WAIT;
-    puts("Mensaje codificado\n");
 }
-void decode(States* state, TipoNodo* Raiz){
+void codificar(char mensaje, char codificado[], int pos, TipoNodo* Raiz, int* flag, char mensajeCodificado[]){
+    if (mensaje == Raiz->Letra){
+        *flag = 1;
+        strcpy(mensajeCodificado, codificado);
+    }
+    if(Raiz != NULL && Raiz->Letra != mensaje && *flag == 0){
+        if(Raiz->izq != NULL){
+            codificado[pos] = '.';
+            codificado[pos + 1] = '\0';
+            codificar(mensaje, codificado, pos + 1, Raiz->izq, flag, mensajeCodificado);
+        }
+        if(Raiz->der != NULL){
+            codificado[pos] = '-';
+            codificado[pos + 1] = '\0';
+            codificar(mensaje, codificado, pos + 1, Raiz->der, flag, mensajeCodificado);
+        }
+    }
+}
+
+void minusculas(char mensaje[]){
+    int i = 0;
+    while (mensaje[i] != '\0'){
+        mensaje[i] = tolower(mensaje[i]);
+        i++;
+    }
+}
+
+void decode(States* state, TipoNodo** Raiz){
     printf("Decodificando mensaje...");
     //print_process_bar(5);
     *state = WAIT;
     puts("Mensaje decodificado\n");
 }
-void free_memory(States* state, TipoNodo* Raiz){
+void free_memory(States* state, TipoNodo** Raiz){
     printf("Liberando memoria...\n");
-    BorrarArbol(Raiz);
+    BorrarArbol(*Raiz);
     *state = FINISH;
 }
-void finish(States* state, TipoNodo* Raiz){
+void finish(States* state, TipoNodo** Raiz){
     printf("Saliendo del programa...\n");
     //print_process_bar(3);
     exit(0);
@@ -129,10 +169,11 @@ void InsertarArbol(TipoNodo **Raiz, double Probabilidad, char Letra){
     Avanza = Nuevo;
     *Raiz = Avanza;
 }
-void BorrarArbol(TipoNodo *Raiz){
+void BorrarArbol(TipoNodo* Raiz){
     if (Raiz != NULL){
         BorrarArbol(Raiz->izq);
         BorrarArbol(Raiz->der);
         free(Raiz);
     }
 }
+
--- state_machine.h
+++ state_machine.h
@@ -6,6 +6,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include <unistd.h>
+#include <ctype.h>
 
 // Enum de los estados de la maquina de estados
 typedef enum states{
@@ -26,20 +27,22 @@ typedef struct def_Arbol{
 // Estructura que contendra etiqueta y funcion
 typedef struct StateMachine{    
     States state;
-    void (*process)(States* state, TipoNodo* Raiz);
+    void (*process)(States* state, TipoNodo** Raiz);
 }StateMachine;
 
 
 
 void InsertarArbol(TipoNodo **Raiz, double Probabilidad, char Letra);
 void BorrarArbol(TipoNodo *Raiz);
+void codificar(char mensaje, char codificado[], int pos, TipoNodo *Raiz, int *flag, char mensajeCodificado[]);
+void minusculas(char mensaje[]);
 // Prototipos de las funciones ejecutables de la maquina de estados
-void load_file(States *state, TipoNodo *Raiz);
-void wait(States* state, TipoNodo* Raiz);
-void encode(States* state, TipoNodo* Raiz);
-void decode(States* state, TipoNodo* Raiz);
-void free_memory(States* state, TipoNodo* Raiz);
-void finish(States* state, TipoNodo* Raiz);
+void load_file(States *state, TipoNodo** Raiz);
+void wait(States* state, TipoNodo** Raiz);
+void encode(States* state, TipoNodo** Raiz);
+void decode(States* state, TipoNodo** Raiz);
+void free_memory(States* state, TipoNodo** Raiz);
+void finish(States* state, TipoNodo** Raiz);
 
 States machine_initialiser(void); // Inicializador de la maquina de estados
 
